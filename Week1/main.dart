41eb1a4ed5b7a2080cde1abaac38e4ff77f34ddf
9T9DEVIL <2020cs152@student.uet.edu.pk>
2023-07-30 14:49:20 +0500

    updated till switch statement

--- Week1/main.dart
+++ Week1/main.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+class Num {
+  int num = 10;
+}
+
 void main() {
   print("Hello");
   stdout.writeln('What is your name: ?');
@@ -24,10 +28,90 @@ void main() {
   print('Dynamic 1: $dyn_1');
   dyn_1 = 235;
   print('I am now Integer : $dyn_1');
+
+//String Tricks
+
+  var nickname = 'Sonu';
+  String myNickName = 'My Nickname is $nickname';
+  print(myNickName);
+
+  //Parsing Data type
+  var intDataOne = int.parse('1');
+  if (intDataOne == 1) {
+    print('It\'s int');
+  } else {
+    print("it's a string");
+  }
+
+  //const type veriable and run-time-checking
+
+  const constantVarInt = 22;
+  const constantVarStr = "Hello I'm Constant";
+  const constantVarBool = true;
+  print(constantVarInt);
+  print(constantVarStr);
+  print(constantVarBool);
+
+  print(constantVarInt.runtimeType);
+  print(constantVarStr.runtimeType);
+  print(constantVarBool.runtimeType);
+
+  //OPERATORS
+
+  int opr_num = 40 + 20;
+  print('First declaration and assignment of OperatorInt: $opr_num');
+  opr_num = opr_num + 20;
+  opr_num = opr_num % 5;
+  print('After Modulus of 5 OperatorInt: $opr_num');
+
+  //OPERATORS RELATIONAL
+
+  if (opr_num == 0) {
+    print('Opr_num is now Zero');
+    opr_num = 100;
+  }
+
+  //OPERATORS UNIARY
+
+  opr_num++;
+  print(opr_num);
+  ++opr_num;
+  print(opr_num);
+  opr_num *= 2;
+  print(opr_num);
+
+  //Logical
+  if (opr_num > 200 || opr_num < 250) {
+    print('Its between 200-250');
+  } else {
+    print('not its not between 200-250');
+    print(opr_num);
+  }
+
+  //Null Aware Operators
+  var n1;
+  var nullAwareCheckInt = n1?.num ?? 15;
+  print(nullAwareCheckInt);
+
+  //Ternary Operators
+  var result = count1 % 2 == 0 ? 'Even' : 'odd';
+  print(result + '  WHICH is   $count1');
+
+  //Switch Statement
+
+  switch (nullAwareCheckInt) {
+    case 10:
+      print('its not null');
+      break;
+    case 15:
+      print('it was null but now assigned with $nullAwareCheckInt');
+      break;
+    default:
+      print('its default');
+  }
 }
 //In-Line Comment
 
 /*
 Block Comment
 */
-
